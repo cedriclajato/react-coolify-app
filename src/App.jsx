a2e0e890ada7930b1e7bb9cda35c0bb25cbe0825
Cedric Lajato <cedric.lajato@gmail.com>
2026-01-06 15:01:03 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+   return (
+      <div style={{ padding: 40, fontFamily: 'sans-serif' }}>
+         <h1>🚀 React App on Coolify</h1>
+         <p>Your deployment is working!</p>
+      </div>
+   );
+}
